91cd97bfba33342950342b9f22f99a151e142035
jonghun4 <xwhdgns@gmail.com>
2024-10-24 13:36:02 +0900

    main Route추가

--- src/JongHun/JongHun.js
+++ src/JongHun/JongHun.js
@@ -1,6 +1,7 @@
 import { SelfMyIntroName } from "./JongHunName";
 import { SelfMyIntro } from "./JongHunIntro";
 import { SelfMyIntroBody } from "./JongHunIntro2";
+import { UseGetApi } from "./UseGetApi";
 
 export function JongHun() {
   return (
