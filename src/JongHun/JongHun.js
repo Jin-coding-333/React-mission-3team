a4245f69599c9b63ae04adf0180f75832d4d8481
jonghun4 <xwhdgns@gmail.com>
2024-10-24 10:40:14 +0900

    add:Link 추가

--- src/JongHun/JongHun.js
+++ src/JongHun/JongHun.js
@@ -2,7 +2,7 @@ import { SelfMyIntroName } from "./JongHunName";
 import { SelfMyIntro } from "./JongHunIntro";
 import { SelfMyIntroBody } from "./JongHunIntro2";
 
-function JongHun() {
+export function JongHun() {
   return (
     <div>
       <SelfMyIntroName />
