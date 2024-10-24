e99a95d6b46cba859b35dec8dad441fcc7bff3c4
Jinhan <wlsgks333@naver.com>
2024-10-24 23:44:27 +0900

    진한 컴포넌트 완성

--- src/Jinhan/JinhanHeader.js
+++ src/Jinhan/JinhanHeader.js
@@ -2,18 +2,19 @@ import { React, useState, useEffect } from "react";
 import './JinhanHeader.css';
 
 function JinhanHeader() {
-  const [img, setImg] = useState;
+  const [img, setImg] = useState();
 
   useEffect(() => {
     fetch('https://picsum.photos/1400/1200')
       .then(response => setImg(response.url));
   }, []);
 
+
   return (
     <header>
       <div id='headerContent'>
         <img src={img} />
-        <div>랜덤 이미지</div>
+        <div>이미지</div>
       </div>
     </header>
   );
